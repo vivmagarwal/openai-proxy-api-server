99e0e5d25e707bdc78edf40a596ca2c53ed41a16
vivmagarwal <vivmagarwal@gmail.com>
2023-03-29 13:11:16 +0530

    fixed dotenv import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const cors = require("cors");
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
