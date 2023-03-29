9c1b71b6ebf6d5b69f6391ad29b1f003e18c2e68
vivmagarwal <vivmagarwal@gmail.com>
2023-03-29 13:09:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+// Set up CORS to restrict requests to specific domains
+const allowedOrigins = [
+  "https://luxury-griffin-3e8f2f.netlify.app",
+  "https://main--poetic-tanuki-765674.netlify.app",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
+// Set up a proxy for requests to the OpenAI API
+const openaiProxy = createProxyMiddleware({
+  target: "https://api.openai.com",
+  changeOrigin: true,
+  pathRewrite: { "^/openai-chat": "/v1/chat/completions" },
+  headers: {
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+  },
+});
+
+app.use("/openai-chat", openaiProxy);
+
+// Start the server
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
