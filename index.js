c58f341b1ecd3a60e76f90f08c0a161269bde205
vivmagarwal <vivmagarwal@gmail.com>
2023-05-18 10:27:52 +0530

    feat: cors restriction removed for testing.

--- index.js
+++ index.js
@@ -6,35 +6,21 @@ dotenv.config();
 
 const app = express();
 
-// Set up CORS to restrict requests to specific domains
-const allowedOrigins = [
-  "https://luxury-griffin-3e8f2f.netlify.app",
-  "https://main--poetic-tanuki-765674.netlify.app",
-];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+// Set up CORS to allow all origins
+app.use(cors());
 
 // Set up a proxy for requests to the OpenAI API
 const openaiProxy = createProxyMiddleware({
   target: "https://api.openai.com",
   changeOrigin: true,
-  pathRewrite: { "^/openai-chat": "/v1/chat/completions" },
   headers: {
-   "Content-Type": "application/json",
+    "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
   },
 });
 
-app.use("/openai-chat", openaiProxy);
+// Use the proxy for all requests
+app.use("*", openaiProxy);
 
 // Start the server
 const port = process.env.PORT || 3000;
